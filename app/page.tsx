faee462852a88377c66a4460acf3349fd3c9bf1b
MatthewHA233 <1528919811@qq.com>
2026-03-21 12:16:56 +0800

    init: minimal Next.js project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">HORIZN</h1>
+    </main>
+  );
+}
